1ee501cdc44e5aeff8cbd4d5af15fa146e52e2bd
STC <STC@DESKTOP-QDRRL6Q>
2024-03-12 14:31:41 +0800

    Add project files.

---
+++ evangelista_activity7/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Form2.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+End Class
+
+
---
+++ evangelista_activity7/Form2.Designer.vb
@@ -0,0 +1,251 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        label1 = New Label()
+        TextBox1 = New TextBox()
+        Label2 = New Label()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        TextBox3 = New TextBox()
+        DateTimePicker1 = New DateTimePicker()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        Button1 = New Button()
+        LinkLabel1 = New LinkLabel()
+        Label7 = New Label()
+        Label8 = New Label()
+        TextBox6 = New TextBox()
+        Label9 = New Label()
+        TextBox7 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' label1
+        ' 
+        label1.AutoSize = True
+        label1.Location = New Point(38, 23)
+        label1.Name = "label1"
+        label1.Size = New Size(59, 15)
+        label1.TabIndex = 0
+        label1.Text = "Firstname"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(38, 41)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(38, 67)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(74, 15)
+        Label2.TabIndex = 0
+        Label2.Text = "Middlename"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(38, 85)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 1
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(38, 117)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(58, 15)
+        Label3.TabIndex = 0
+        Label3.Text = "Lastname"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(38, 135)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 1
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Location = New Point(38, 179)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(200, 23)
+        DateTimePicker1.TabIndex = 2
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(38, 161)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(55, 15)
+        Label4.TabIndex = 3
+        Label4.Text = "Birthdate"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(248, 254)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(60, 15)
+        Label5.TabIndex = 4
+        Label5.Text = "Username"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(524, 254)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(57, 15)
+        Label6.TabIndex = 4
+        Label6.Text = "Password"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(177, 272)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(179, 23)
+        TextBox4.TabIndex = 5
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(459, 272)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(179, 23)
+        TextBox5.TabIndex = 5
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(361, 314)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(85, 24)
+        Button1.TabIndex = 6
+        Button1.Text = "Save"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.Location = New Point(339, 350)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(142, 15)
+        LinkLabel1.TabIndex = 7
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "Already have an account?"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(38, 23)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(59, 15)
+        Label7.TabIndex = 0
+        Label7.Text = "Firstname"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(38, 67)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(74, 15)
+        Label8.TabIndex = 0
+        Label8.Text = "Middlename"
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(38, 41)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(100, 23)
+        TextBox6.TabIndex = 1
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(38, 117)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(58, 15)
+        Label9.TabIndex = 0
+        Label9.Text = "Lastname"
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(38, 85)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(100, 23)
+        TextBox7.TabIndex = 1
+        ' 
+        ' Form2
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(LinkLabel1)
+        Controls.Add(Button1)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(DateTimePicker1)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox7)
+        Controls.Add(Label9)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox6)
+        Controls.Add(Label3)
+        Controls.Add(Label8)
+        Controls.Add(TextBox1)
+        Controls.Add(Label7)
+        Controls.Add(Label2)
+        Controls.Add(label1)
+        Name = "Form2"
+        Text = "Form2"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox7 As TextBox
+End Class
